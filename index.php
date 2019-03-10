d29ba984fe8726b06823da73df3e6d9cbdf49966
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-09 21:14:42 -0500

    Create database, Create dummy test table, populate table, connect to the database

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<!DOCTYPE html>
+<?php
+
+// variable to hold connection to our database
+$con = mysqli_connect("localhost","root","","flock");
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+$query = mysqli_query($con, "INSERT INTO test VALUES('','Mehdi')");
+?>
 <html>
 <head>
-	<title></title>
+	<title>Flock</title>
 </head>
 <body>
 
