edb48a87197ce2910840c10a63ffa264c4d6a435
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-13 15:54:06 -0400

    Sort error messages

--- register.php
+++ register.php
@@ -18,17 +18,17 @@ $em2 = ""; //email 2
 $password = ""; //password
 $password2 = ""; //password 2
 $date = ""; //Sign up date
-$error_array=""; //Holds error messages
+$error_array=array(); //Holds error messages
 
 // if the register button is set start handling the form
 if(isset($_POST['register_button'])) {
 
 	// Registration form values
 	// strip_tags is a security measure that takes away html tags that might
-	
-		/// First name ///
-
 	// add some unwanted code to your website
+
+
+		/// First name ///
 	$fname = strip_tags($_POST['reg_fname']);
 
 	// removes spaces. str_replace(search, replace, subject)
@@ -85,6 +85,8 @@ if(isset($_POST['register_button'])) {
 
 	if($em == $em2) {
 	// Check if email is in valid format
+		//filter_var(variable) similar to strip_tags but also filter and
+		//sanitizes the variable and strips the script tags
 		if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
 			$em = filter_var($em, FILTER_VALIDATE_EMAIL);
 
@@ -98,38 +100,46 @@ if(isset($_POST['register_button'])) {
 			$num_rows = mysqli_num_rows($e_check);
 
 			if($num_rows > 0) {
-				echo "Email already in use";
+				//echo "Email already in use";
+				array_push($error_array, "Email already in use<br>");
 			}
 		}
 		else {
-			echo "Invalid format";
+			//echo "Invalid format";
+			array_push($error_array, "Invalid email format<br>");
 		}
 
 	}
 	else {
-		echo "Emails don't match";
+		//echo "Emails don't match";
+		array_push($error_array, "Emails don't match<br>");
 	}
 
 	if(strlen($fname) > 25 || strlen($fname)<2 ) {
-		echo "Your first name must be between 2 and 25 characters";
+		//echo "Your first name must be between 2 and 25 characters";
+		array_push($error_array,
 	}
 
 	if(strlen($lname) > 25 || strlen($lname)<2 ) {
-		echo "Your last name must be between 2 and 25 characters";
+		//echo "Your last name must be between 2 and 25 characters";
+		array_push($error_array, "Your last name must be between 2 and 25 characters<br>");
 	}
 
 	if($password != $password2) {
-		echo "Your passwords do not match";
+		//echo "Your passwords do not match";
+		array_push($error_array, "Your passwords do not match<br>");
 	}
 
 	else {
 		if(preg_match('/[^A-Za-z0-9]/', $password)) {
-			echo "Your password can only contain English characters or numbers";
+			//echo "Your password can only contain English characters or numbers";
+			array_push($error_array, "Your password can only contain English characters or numbers<br>");
 		}
 	}
 
 	if(strlen($password) > 30 || strlen($password) < 5 ) {
-		echo "Your password must be between 5 and 30 characters";
+		//echo "Your password must be between 5 and 30 characters";
+		array_push($error_array,"Your password must be between 5 and 30 characters<br>");
 	}
 
 
