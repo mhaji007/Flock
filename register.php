33b807d99ff2ddbf88aab99226b3d473e21b7992
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-16 17:20:42 -0400

    Create the login form

--- register.php
+++ register.php
@@ -13,6 +13,16 @@ require 'includes/form_handlers/register_handler.php';
 </head>
 <body>
 
+	<form action="register.php" method="POST">
+
+		<input type="email" name="log_email" placeholder="Email Address">
+		<br>
+		<input type="password" name="log_password" placeholder="Password">
+		<br>
+		<input type="submit" name="login_button" value="Login">
+
+
+	</form>
 
 	<form action="register.php" method="POST">
 		<input type="text" name="reg_fname" placeholder="First Name" value="<?php
