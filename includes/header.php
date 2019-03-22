3f559092d35d516a388bca4b263a444eb202fe9f
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-22 19:57:12 -0400

    Create the top bar

--- includes/header.php
+++ includes/header.php
@@ -11,10 +11,30 @@ else { //send them back to the register page
 
 ?>
 <html>
+
 <head>
 	<title>Flock</title>
+
+	<!-- Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+
+	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
 </head>
+
 <body>
+	<div class = "top_bar">
+
+		<div class = "logo">
+			
+			<a href="index.php">Flock!</a>
+
+		</div>
+		
+
+
+
+	</div>
