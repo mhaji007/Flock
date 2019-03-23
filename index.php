51fd25426d8dda649a96621963ee463beeb8f0e1
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-23 14:54:07 -0400

    Style the user details panel

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 include ("includes/header.php");
 //session_destroy();
 ?>
-	<div vlass="user_details column">
-		<a href="#"><img src="<?php echo $user['profile_pic'] ?>"></a>
+	<div class="user_details column">
+		<a href="#"><img src="<?php echo $user['profile_pic']; ?>"></a>
 	</div>
 
 
 
-
-	</div>	
+	<!-- closing tag for the wrapper from header.php -->
+	</div>
 </body>
 </html>
