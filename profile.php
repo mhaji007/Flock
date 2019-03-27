2fcadd756b267a28993e62f98180625e51716acb
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-27 18:08:56 -0400

    Add the htaccess file

---
+++ profile.php
@@ -0,0 +1,17 @@
+<?php
+include ("includes/header.php");
+//session_destroy();
+?>
+
+
+	<div class="main_column column">
+		This is a profile page!
+		
+	</div>
+
+
+
+	<!-- closing tag for the wrapper from header.php -->
+	</div>
+</body>
+</html>
