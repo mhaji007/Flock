a862b0ce842359b4d4dd0cf7dc0206675d404131
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-04-07 20:58:43 -0400

    Fix type in includes

--- includes/handlers/ajax_load_posts.php
+++ includes/handlers/ajax_load_posts.php
@@ -1,7 +1,7 @@
 <?php
 include("../../config/config.php");
 include("../classes/User.php");
-include("../classes/Posts.php");
+include("../classes/Post.php");
 
 $limit = 10; // Number of posts to be loaded per call
 
